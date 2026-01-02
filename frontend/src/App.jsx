deeced15226e2cc3fd89dc38c159acf36b424263
bzdsl <1851060093@e.tlu.edu.vn>
2026-01-02 11:55:07 +0700

    convert home and checkout to async await

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,9 +9,11 @@ import { useEffect, useState } from "react";
 function App() {
   const [cart, setCart] = useState([]);
   useEffect(() => {
-    axios.get("/api/cart-items?expand=product").then((response) => {
+    const fetchAppData = async () => {
+      const response = await axios.get("/api/cart-items?expand=product");
       setCart(response.data);
-    });
+    };
+    fetchAppData();
   }, []);
   return (
     <Routes>
